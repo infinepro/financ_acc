ccc5a6ea30d013ad3531859209e1d7ccc26570dd
Maksimka <mickey_6@mail.ru>
2020-02-13 23:41:15 +0300

    bot config, BOT WORK, BOT have LOGGER

--- src/main/java/ru/maksimka/jb/MainBot.java
+++ src/main/java/ru/maksimka/jb/MainBot.java
@@ -1,7 +1,7 @@
 package ru.maksimka.jb;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.maksimka.jb.bot.Bot;
@@ -10,7 +10,7 @@ import static ru.maksimka.jb.configurations.SpringContext.getContext;
 
 public class MainBot {
 
-    private static Logger log = LoggerFactory.getLogger(MainBot.class);;
+    private static final Logger log = LogManager.getLogger("BOT INFO");
 
     public static void main(String[] args) {
 
@@ -19,7 +19,7 @@ public class MainBot {
         try {
             log.info("Bot init....");
             botsApi.registerBot(getContext().getBean(Bot.class));
-            log.info("Bot is worjing...and he is waiting requests");
+            log.info("Bot is working...and he is waiting requests...");
 
         } catch (TelegramApiException e) {
             e.printStackTrace();

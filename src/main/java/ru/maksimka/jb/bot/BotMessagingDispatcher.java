9a6861288d55cbe4d6b066cf40391ebd49c36813
Maksimka <mickey_6@mail.ru>
2020-02-16 23:55:55 +0300

    add table and entity

--- src/main/java/ru/maksimka/jb/bot/BotMessagingDispatcher.java
+++ src/main/java/ru/maksimka/jb/bot/BotMessagingDispatcher.java
@@ -9,6 +9,9 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import ru.maksimka.jb.bot.Controllers.AllTheRestHandler;
+import ru.maksimka.jb.dto.TelegramUserDto;
+
+import java.util.HashMap;
 
 import static ru.maksimka.jb.configurations.SpringContext.getContext;
 
@@ -17,11 +20,26 @@ public class BotMessagingDispatcher {
 
     private static final Logger LOGGER = LogManager.getLogger("BOT INFO");
 
+    //пока не знаю другого варианта хранить авторизованных и нет юзеров, потом разберусь)
+    private HashMap<Long, TelegramUserDto> usersList;
+
     @Qualifier("start")
     private final InlineKeyboardMarkup startKeyboard;
 
     public BotMessagingDispatcher(InlineKeyboardMarkup startKeyboard) {
         this.startKeyboard = startKeyboard;
+        usersList = new HashMap<>();
+    }
+//todo: придётся переделывать на абилити бота
+    private void userVerification(Update update) {
+
+        long chatId;
+        if (update.hasCallbackQuery()) {
+            chatId = update.getCallbackQuery().getMessage().getChatId();
+        } else {
+            chatId = update.getMessage().getChatId();
+        }
+
     }
 
     public SendMessage getSendMessageController(Update update) {
@@ -35,14 +53,16 @@ public class BotMessagingDispatcher {
                 handler = getContext().getBean(Handler.class, handlerName);
             } catch (BeansException e) {
                 handler = getContext().getBean(AllTheRestHandler.class);
-            } return handler.execute(update);
+            }
+            return handler.execute(update);
         } else if (update.hasMessage()) {
             try {
                 handlerName = update.getMessage().getText();
                 handler = getContext().getBean(Handler.class, handlerName);
             } catch (BeansException e) {
                 handler = getContext().getBean(AllTheRestHandler.class);
-            } return handler.execute(update);
+            }
+            return handler.execute(update);
         }
         return new SendMessage()
                 .setText("Человек, ты прислал мне какую то ерунду...")
--- src/main/java/ru/maksimka/jb/dto/TelegramUserDto.java
+++ src/main/java/ru/maksimka/jb/dto/TelegramUserDto.java
@@ -1,4 +1,23 @@
 package ru.maksimka.jb.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+//lombok
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@With
+
 public class TelegramUserDto {
+
+
+
+    private int id;
+    private String nameUser;
+    private long chatId;
+    private String phoneNumber;
+    private String email;
 }
